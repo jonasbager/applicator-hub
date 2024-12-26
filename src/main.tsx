0381b9ab9a93faa20ecb8be48759ce4a9fcb925d
Jonas Bager <jonasbager@Jonass-MacBook-Air.local>
2024-12-25 20:50:48 -0600

    fix: update clerk configuration with env variables

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider 
       publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      afterSignInUrl={import.meta.env.VITE_CLERK_AFTER_SIGN_IN_URL}
+      afterSignUpUrl={import.meta.env.VITE_CLERK_AFTER_SIGN_UP_URL}
+      afterSignOutUrl={import.meta.env.VITE_CLERK_AFTER_SIGN_OUT_URL}
     >
       <BrowserRouter>
         <App />
